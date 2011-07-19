209d7a23c5c3485378709643e59d378d01c3a1ed
walkline <walkline.ua@gmail.com>
2011-07-19 21:10:32 +0300

    Fix encoding in windows.

--- src/parsers/circle/circleparseonline.cpp
+++ src/parsers/circle/circleparseonline.cpp
@@ -33,7 +33,11 @@ int CircleParseOnline::ParseRealmsCount()
 
 void CircleParseOnline::ParseNames()
 {
+#ifdef Q_OS_WIN32
+    QTextCodec::setCodecForTr(QTextCodec::codecForName("Windows-1251"));
+#else
     QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
+#endif
     int i = 0;
     if (parser.SearchSegments("<div class=\"info\">","<br>"))
         do {
